66670bc8416c11d43c415b77a7682f10c689a885
yang <1045101294@qq.com>
2022-04-24 16:01:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/Demo01.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @Description: 测试
+ * @Author: june
+ * @Date: 2022/4/24
+ * @Version: 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
